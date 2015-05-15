931d0ac7741888459963df2915288620af34cff6
Adam Oswalt <adam.oswalt@gmail.com>
2015-05-15 11:13:49 -0500

    Initial work on pulling project information from repository website and sql database.

--- projects.php
+++ projects.php
@@ -1,23 +1,50 @@
 <?php
+	$sql_server = 'localhost';
+	$sql_username = 'root';
+	$sql_password = 'root';
+	$sql_dbname = 'portfolio';
 	
-	$title = 'Entry Title';
-	$languages = 'Java';
-	$description = 'Curabitur id ornare eros, eget suscipit tortor. Vestibulum quis libero eu quam venenatis elementum et vitae nisi. Fusce ultrices lacinia diam sit amet accumsan.';
-	$source_url = 'https://bitbucket.org/aoswalt';
-	$repo_id = '%7B81dd4861-ab6f-4907-ab6e-084bbc24a93b%7D';
-	$image_file = 'http://upload.wikimedia.org/wikipedia/commons/3/30/Googlelogo.png';
-	$thumb_file = 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Googlelogo.png/320px-Googlelogo.png';
+	$conn = new mysqli($sql_server, $sql_username, $sql_password, $sql_dbname);
+	
+	//TODO is any error handling neccessary?
+	if($conn->connect_error) {
+		die('Connection failed: '.$conn->connect_error);
+	}
+	
+	$query = 'SELECT image_file, thumb_file FROM project_images WHERE repo_id LIKE ';
+	
+	//TODO add a message if no repos found
+	$public_bb_repos = file_get_contents('https://api.bitbucket.org/2.0/repositories/aoswalt');
+	$bb_data = json_decode($public_bb_repos);
+	
+	$repo = $bb_data->{'values'}[0];
+	$title = $repo->{'name'};
+	$language = $repo->{'language'};
+	$description = $repo->{'description'};
+	$source_url = $repo->{'links'}->{'html'}->{'href'};
+	$repo_id = $repo->{'uuid'};		//'{f2138537-f824-4475-8d02-88a2d9367f5f}';		// bb = uuid, gh = numeric
+	
+	$commits_json = file_get_contents('https://api.bitbucket.org/2.0/repositories/aoswalt/'.$repo_id.'/commits');
+	$commits = json_decode($commits_json);
+	$last_commit = strtotime($commits->{'values'}[0]->{'date'});	// saved directly as time for sorting
 	
 	
-	echo '
+	$query_result = $conn->query($query.'"'.$repo_id.'"');
+	$row = $query_result->fetch_assoc();
+	$image_file = $row["image_file"];
+	$thumb_file = $row["thumb_file"];
+	
+	$conn->close();
+	
+	echo('
 		<li class="entry">
 			<h2>'.$title.'</h2>
 			<a href="'.$image_file.'" target="_blank"><img src="'.$thumb_file.'" width="250px" height="150px" /></a>
 			<div class="entry-info">
-				<p><b>Languages: </b>'.$languages.'</p>
+				<p><b>Language: </b>'.$language.'</p>
 				<p class="entry-description">'.$description.'</p>
-				<p class="links"><a href="'.$source_url.'">Source Code</a><a href="'.$repo_id.'">Download</a></p>
+				<p class="source"><a href="'.$source_url.'">Source Code</a><span>Last commit: '.date('Y-m-d', $last_commit).'</span></p>
 			</div>
 		</li>
-		';
+		');
 	?>
