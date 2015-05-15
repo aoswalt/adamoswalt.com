fb49ff4854cddd7eb96ce44dd5cdfe9d59023ec8
Adam Oswalt <adam.oswalt@gmail.com>
2015-05-15 15:06:06 -0500

    Moved to object based data handling for unifying different sources.

--- projects.php
+++ projects.php
@@ -1,4 +1,15 @@
 <?php
+	class RepoData {
+		public $title;
+		public $language;
+		public $description;
+		public $source_url;
+		public $last_commit;
+		public $image_file;
+		public $thumb_file;
+		public $repo_id;
+	}
+	
 	$sql_server = 'localhost';
 	$sql_username = 'root';
 	$sql_password = 'root';
@@ -6,44 +17,54 @@
 	
 	$conn = new mysqli($sql_server, $sql_username, $sql_password, $sql_dbname);
 	
-	//TODO is any error handling neccessary?
 	if($conn->connect_error) {
+		//TODO proper error handling for page to still load
 		die('Connection failed: '.$conn->connect_error);
 	}
 	
-	$query = 'SELECT image_file, thumb_file FROM project_images WHERE repo_id LIKE ';
+	$query = 'SELECT * FROM project_info WHERE repo_id LIKE ';
 	
-	//TODO add a message if no repos found
+	
+	$repos = array();
+	
 	$public_bb_repos = file_get_contents('https://api.bitbucket.org/2.0/repositories/aoswalt');
 	$bb_data = json_decode($public_bb_repos);
+	$data = $bb_data->{'values'}[0];
 	
-	$repo = $bb_data->{'values'}[0];
-	$title = $repo->{'name'};
-	$language = $repo->{'language'};
-	$description = $repo->{'description'};
-	$source_url = $repo->{'links'}->{'html'}->{'href'};
-	$repo_id = $repo->{'uuid'};		//'{f2138537-f824-4475-8d02-88a2d9367f5f}';		// bb = uuid, gh = numeric
 	
-	$commits_json = file_get_contents('https://api.bitbucket.org/2.0/repositories/aoswalt/'.$repo_id.'/commits');
+	$repo = new RepoData();
+	
+	$repo->language = $data->{'language'};	//TODO map of prettier language entries?
+	$repo->description = $data->{'description'};
+	$repo->source_url = $data->{'links'}->{'html'}->{'href'}.'/src';	// go directly to source code in bb without changing settings
+	$repo->repo_id = $data->{'uuid'};		// bb = uuid, gh = numeric
+	
+	$commits_json = file_get_contents('https://api.bitbucket.org/2.0/repositories/aoswalt/'.$repo->repo_id.'/commits');
 	$commits = json_decode($commits_json);
-	$last_commit = strtotime($commits->{'values'}[0]->{'date'});	// saved directly as time for sorting
+	$repo->last_commit = strtotime($commits->{'values'}[0]->{'date'});	// saved directly as time for sorting
 	
 	
-	$query_result = $conn->query($query.'"'.$repo_id.'"');
+	$query_result = $conn->query($query.'"'.$repo->repo_id.'"');
 	$row = $query_result->fetch_assoc();
-	$image_file = $row["image_file"];
-	$thumb_file = $row["thumb_file"];
+	$repo->title = $row['name'];		// github doesn't store a presentable name	//TODO fallback to $title = $repo->{'name'};
+	$repo->image_file = $row['image_file'];
+	$repo->thumb_file = $row['thumb_file'];
+	
+	array_push($repos, $repo);
 	
 	$conn->close();
 	
+	$i = 0;
+	//TODO add a message if no repos found
+	
 	echo('
 		<li class="entry">
-			<h2>'.$title.'</h2>
-			<a href="'.$image_file.'" target="_blank"><img src="'.$thumb_file.'" width="250px" height="150px" /></a>
+			<h2>'.$repos[$i]->title.'</h2>
+			<a href="'.$repos[$i]->image_file.'" target="_blank"><img src="'.$repos[$i]->thumb_file.'" width="250px" height="150px" /></a>
 			<div class="entry-info">
-				<p><b>Language: </b>'.$language.'</p>
-				<p class="entry-description">'.$description.'</p>
-				<p class="source"><a href="'.$source_url.'">Source Code</a><span>Last commit: '.date('Y-m-d', $last_commit).'</span></p>
+				<p><b>Language: </b>'.$repos[$i]->language.'</p>
+				<p class="entry-description">'.$repos[$i]->description.'</p>
+				<p class="source"><a href="'.$repos[$i]->source_url.'">Source Code</a><span>Last commit: '.date('Y-m-d', $repos[$i]->last_commit).'</span></p>
 			</div>
 		</li>
 		');
