f775f0100957e2c206558a169d2cb8c67c8770f4
Adam Oswalt <adam.oswalt@gmail.com>
2015-06-01 13:41:09 -0500

    Changed links to be relative.

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 	echo('
 		<header id="nav-bar">
 			<ul id="nav-links">
-				<li><a href="/angergames/index.php">PROJECTS</a></li>
-				<li><a href="/angergames/about.php">ABOUT ME</a></li>
-				<li><a href="/angergames/resume.php">RÉSUMÉ</a></li>
-				<li><a href="/angergames/contact.php">CONTACT</a></li>
+				<li><a href="./index.php">PROJECTS</a></li>
+				<li><a href="./about.php">ABOUT ME</a></li>
+				<li><a href="./resume.php">RÉSUMÉ</a></li>
+				<li><a href="./contact.php">CONTACT</a></li>
 			</ul>
 		</header>
 		');
