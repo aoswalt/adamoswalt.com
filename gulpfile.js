ad012cfaffed3bd084478101533dcd70377cd630
Adam Oswalt <adam.oswalt@gmail.com>
2016-07-26 18:23:36 -0500

    basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const gulp = require('gulp')
+const connect = require('gulp-connect')
+const del = require('del')
+const runSequence = require('run-sequence')
+const sass = require('gulp-sass')
+
+const sourcePath = './src'
+const publicPath = './public'
+const sassRelativePath = '/style'
+
+const allSourcePath = `${sourcePath}/**/*`
+const allPublicPath = `${publicPath}/**/*`
+const allSassPath = `${sourcePath}${sassRelativePath}/**/*.scss`
+const sassEntryPath = `${sourcePath}${sassRelativePath}/main.scss`
+const staticPath = [allSourcePath, `!${allSassPath}`]
+
+
+// CLEANERS
+gulp.task('clean:all', () => del(allPublicPath))
+
+
+// COPIERS
+gulp.task('static:copy', () =>
+  gulp.src(staticPath)
+    .pipe(gulp.dest(publicPath)))
+
+
+// COMPILERS
+gulp.task('sass:compile', () => {
+  gulp.src(sassEntryPath)
+    .pipe(sass())
+      .on('error', sass.logError)
+    .pipe(gulp.dest(`${publicPath}/styles`))
+})
+
+
+// LIVERELOAD
+gulp.task('connect', () =>
+  connect.server({
+    root: publicPath,
+    livereload: true
+  }))
+
+gulp.task('livereload', () =>
+  gulp.src(allPublicPath)
+    .pipe(connect.reload()))
+
+
+// WATCHERS
+gulp.task('watch', () =>
+  runSequence(
+    'clean:all',
+    'build',
+    [
+      'static:watch',
+      'sass:watch',
+      'livereload:watch',
+      'connect'
+    ]
+  ))
+
+gulp.task('static:watch', () =>
+  gulp.watch(staticPath, ['static:copy']))
+
+gulp.task('sass:watch', () =>
+  gulp.watch(allSassPath, ['sass:compile']))
+
+gulp.task('livereload:watch', () =>
+  gulp.watch(allPublicPath, ['livereload']))
+
+
+// BUILDERS
+gulp.task('build', ['sass:compile', 'static:copy'])
